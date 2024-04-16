1aa15b50a824816b4aa853846c8101bc804350c5
@BrandonWedu <brandon.education@proton.me>
2024-04-16 11:59:39 -0400

    Implement menu

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,25 +1,70 @@
 package edu.temple.myapplication
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
+    var timerBinder : TimerService.TimerBinder? = null
+
+    private val serviceConnection = object: ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            timerBinder = service as TimerService.TimerBinder
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            timerBinder = null
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.startButton).setOnClickListener {
+        bindService(
+            Intent(this, TimerService::class.java),
+            serviceConnection,
+            BIND_AUTO_CREATE
+        )
 
+        findViewById<Button>(R.id.startButton).setOnClickListener {
+            timerBinder?.start(100)
         }
 
         findViewById<Button>(R.id.pauseButton).setOnClickListener {
-
+            timerBinder?.pause()
         }
-        
+
         findViewById<Button>(R.id.stopButton).setOnClickListener {
-
+            timerBinder?.stop()
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unbindService(serviceConnection)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_start -> timerBinder?.start(100)
+            R.id.action_pause -> timerBinder?.pause()
+            R.id.action_stop -> timerBinder?.stop()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
